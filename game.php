2a825ee8e87793d5caa058fa74612e5eb41ce7dd
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2022-05-09 15:03:45 +0200

    Update game.php

--- game.php
+++ game.php
@@ -224,7 +224,7 @@ include("config/configbdd.php");
                 this.foret = this.add.image(0, 0, 'foretile').setInteractive();
                 Phaser.Display.Align.In.Center(this.foret, this.add.zone(window.innerWidth/2, window.innerHeight/2, window.innerWidth, window.innerHeight));
                 this.foret.setX(this.foret.x+692);
-                this.foret.setY(this.foret.y+300);
+                this.foret.setY(this.foret.y+302);
                 this.foret.once('pointerdown', function(){
                     if(this.data.get('unlock').includes('foret')) {
                         if(this.scene.isSleeping('foretScene')) {                        
