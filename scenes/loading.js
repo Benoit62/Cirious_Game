2c00e826fc48fff1fe734fcf3453eac58e3528bb
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2022-05-17 10:02:37 +0200

    Petit trie

--- scenes/loading.js
+++ scenes/loading.js
@@ -44,22 +44,15 @@ class Loading extends Phaser.Scene {
 
 
         //Animaux
-        this.load.spritesheet('animaldead', 'assets/build/animaldead.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('pig', 'assets/build/pig_spritesheet.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('cow', 'assets/build/cow_spritesheet.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('sheep', 'assets/build/sheep_spritesheet.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('salmon', 'assets/build/salmon.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('renne', 'assets/build/renne.png', { frameWidth: 416, frameHeight: 416 });
-        this.load.spritesheet('chamel', 'assets/build/chamel.png', { frameWidth: 416, frameHeight: 416 });
-        
+        for(let i of getByType('animal')) {
+            this.load.spritesheet(i.tag, 'assets/animal/'+i.tag+'.png', { frameWidth: 416, frameHeight: 416 });
+        }
 
         // Champs et cultures
         this.load.spritesheet('weeds', 'assets/culture/weeds.png', { frameWidth: 288, frameHeight: 416 });
-        this.load.spritesheet('carrot', 'assets/culture/carrot.png', { frameWidth: 288, frameHeight: 416 });
-        this.load.spritesheet('mais', 'assets/culture/mais.png', { frameWidth: 288, frameHeight: 416 });
-        this.load.spritesheet('ble', 'assets/culture/ble.png', { frameWidth: 288, frameHeight: 416 });
-        this.load.spritesheet('riz', 'assets/culture/riz.png', { frameWidth: 288, frameHeight: 416 });
-        this.load.spritesheet('colza', 'assets/culture/colza.png', { frameWidth: 288, frameHeight: 416 });
+        for(let i of getByType('plant')) {
+            this.load.spritesheet(i.tag, 'assets/culture/'+i.tag+'.png', { frameWidth: 288, frameHeight: 416 });
+        }
         
 
         //Player
@@ -95,55 +88,56 @@ class Loading extends Phaser.Scene {
         
         this.load.image('bull-button', 'assets/menu/bull.png');
         
-        this.load.image("cow-button", "assets/menu/cow.png"); 
-        this.load.image("pig-button", "assets/menu/pig.png"); 
-        this.load.image("sheep-button", "assets/menu/sheep.png"); 
-        this.load.image("bison-button", "assets/menu/bison.png"); 
-        this.load.image("chamel-button", "assets/menu/chameau.png"); 
-        this.load.image("renne-button", "assets/menu/rennes.png");
-        this.load.image('salmon-button', 'assets/menu/salmon.png');
 
-        this.load.image("labor-button", "assets/menu/labourer.png"); 
+        //Boutton des animaux
+        for(let i of getByType('animal')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
+
+        /*this.load.image("labor-button", "assets/menu/labourer.png"); 
         this.load.image("water-button", "assets/menu/water.png"); 
         this.load.image("tank-button", "assets/menu/tank.png"); 
-        this.load.image("solaire-button", "assets/menu/solaire.png"); 
+        this.load.image("solaire-button", "assets/menu/solaire.png"); */
+
+        //Bouttons des structures
+        for(let i of getByType('struct')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
+
+        //Bouttons des champs
+        for(let i of getByType('field')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
         
-        this.load.image("carrot-button", "assets/menu/carrot.png"); 
-        this.load.image("ble-button", "assets/menu/ble.png"); 
-        this.load.image("avoine-button", "assets/menu/avoine.png"); 
-        this.load.image("champi-button", "assets/menu/champignons.png"); 
-        this.load.image("colza-button", "assets/menu/colza.png"); 
-        this.load.image("herbe-button", "assets/menu/herb.png"); 
-        this.load.image("lichen-button", "assets/menu/lichens.png"); 
-        this.load.image("mais-button", "assets/menu/mais.png"); 
-        this.load.image("pdt-button", "assets/menu/pdt.png"); 
-        this.load.image("saxaoul-button", "assets/menu/saxaoul.png"); 
-        this.load.image("soja-button", "assets/menu/soja.png"); 
-        this.load.image("tamaris-button", "assets/menu/tamaris.png"); 
-        this.load.image("riz-button", "assets/menu/riz.png"); 
+        
+        //Boutton des cultures
+        for(let i of getByType('plant')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
 
 
-        this.load.image('fumier-button', 'assets/menu/fumier.png');
-        this.load.image('compost-button', 'assets/menu/compost.png');
-        this.load.image('vert-button', 'assets/menu/vert.png');
-        this.load.image('engrais-button', 'assets/menu/engrais.png');
+        //Boutton des engrais
+        for(let i of getByType('fertility')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
 
-        this.load.image('lutte_chimique-button', 'assets/menu/lutte_chimique.png');
-        this.load.image('physique-button', 'assets/menu/lutte_physique.png');
-        this.load.image('drone-button', 'assets/menu/drone.png');
-        this.load.image('drone2-button', 'assets/menu/drone2.png');
-        this.load.image('humain-button', 'assets/menu/humain.png');
-        this.load.image('biologic-button', 'assets/menu/lutte_bio.png');
-        this.load.image('biopesticid-button', 'assets/menu/biopesticides.png');
 
-        this.load.image('fourrages-button', 'assets/menu/fourrages.png');
-        this.load.image('grains-button', 'assets/menu/grains.png');
-        this.load.image('ration-button', 'assets/menu/ration.png');
-        this.load.image('indus-button', 'assets/menu/indus.png');
+        //Boutton des luttes
+        for(let i of getByType('health')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
 
-        this.load.image('barbare-button', 'assets/menu/barbare.png');
-        this.load.image('propre-button', 'assets/menu/propre.png');
-        this.load.image('rituel-button', 'assets/menu/rituel.png');
+
+        //Boutton des nourritures
+        for(let i of getByType('meal')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
+
+
+        //Boutton des abbatages
+        for(let i of getByType('sell')) {
+            this.load.image(i.tag+'-button', 'assets/menu/'+i.tag+'.png');
+        }
 
 
         // Chargement des autres assets du menu
@@ -158,13 +152,11 @@ class Loading extends Phaser.Scene {
 
 
         //Chargement ds icones de recherche
-        this.load.image('fertility-search', 'assets/search/fertility.png');
         this.load.image('fumier-search', 'assets/search/fumier.png');
         this.load.image('compost-search', 'assets/search/compost.png');
         this.load.image('vert-search', 'assets/search/vert.png');
         this.load.image('engrais-search', 'assets/search/engrais.png');
 
-        this.load.image('health-search', 'assets/search/health.png');
         this.load.image('lutte_chimique-search', 'assets/search/lutte_chimique.png');
         this.load.image('physique-search', 'assets/search/lutte_physique.png');
         this.load.image('drone-search', 'assets/search/drone.png');
@@ -173,7 +165,6 @@ class Loading extends Phaser.Scene {
         this.load.image('biologic-search', 'assets/search/lutte_bio.png');
         this.load.image('biopesticid-search', 'assets/search/biopesticides.png');
 
-        this.load.image('meal-search', 'assets/search/meal.png');
         this.load.image('fourrages-search', 'assets/search/fourrages.png');
         this.load.image('grains-search', 'assets/search/grains.png');
         this.load.image('ration-search', 'assets/search/ration.png');
@@ -182,6 +173,33 @@ class Loading extends Phaser.Scene {
         this.load.image('barbare-search', 'assets/search/barbare.png');
         this.load.image('propre-search', 'assets/search/propre.png');
         this.load.image('rituel-search', 'assets/search/rituel.png');
+
+        //Boutton des engrais
+        this.load.image('fertility-search', 'assets/search/fertility.png');
+        for(let i of getByType('fertility')) {
+            this.load.image(i.tag+'-search', 'assets/search/'+i.tag+'.png');
+        }
+
+
+        //Boutton des luttes
+        this.load.image('health-search', 'assets/search/health.png');
+        for(let i of getByType('health')) {
+            this.load.image(i.tag+'-search', 'assets/search/'+i.tag+'.png');
+        }
+
+
+        //Boutton des nourritures
+        this.load.image('meal-search', 'assets/search/meal.png');
+        for(let i of getByType('meal')) {
+            this.load.image(i.tag+'-search', 'assets/search/'+i.tag+'.png');
+        }
+
+
+        //Boutton des abbatages
+        this.load.image('sell-search', 'assets/search/sellAnimal.png');
+        for(let i of getByType('sell')) {
+            this.load.image(i.tag+'-search', 'assets/search/'+i.tag+'.png');
+        }
 
 
     }
