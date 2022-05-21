8e1657ae43c55ac483824287bcbaf4ee0d403e2c
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2022-05-21 15:33:43 +0200

    Update loading.js

--- scenes/loading.js
+++ scenes/loading.js
@@ -96,8 +96,8 @@ class Loading extends Phaser.Scene {
             });
 
             // Data
-            this.registry.set('unlock', ['europe']);
-            this.registry.set('lock', ['aride', 'tropic', 'polaire']);
+            this.registry.set('unlock', ['europe','aride', 'tropic', 'polaire']);
+            this.registry.set('lock', []);
 
 
             progressBar.destroy();
